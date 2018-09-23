0265150ddcf3492e73cace0b4f0a8cf78e26b6ec
Yernar_Akimzhanov <ernarkazakh07@gmail.com>
2018-09-23 10:58:12 -0400

    Made articles and discussions

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -16,14 +16,34 @@
       </div>
 <!--       <hr style="clear:both;"/> -->
       <div>
-        <h3>Articles</h3>
-        <h3>Discussions</h3>
-        <h3>People</h3>
+        <a v-on:click="runReddit()">Articles</a>
+        <a v-on:click='runAskhn()'>Discussions</a>
+        <a >People</a>
       </div>
       <div>
-        <el-row :gutter='20'>
+        <el-row :gutter='20' v-if='reddit_list'>
+          <el-col :span='8' v-for="item in reddit_list">
+            <a :href="item.url">
+            <el-card :body-style="{ padding: '0px' }">
+              <img src='https://media.wired.com/photos/5a55a95af41e4c2cd9ee6cd8/191:100/pass/Medium-logo-canvas.jpg'>
+              <div style="padding: 14px;">
+                <span>{{item.title}}</span>
+                <span>{{item.fulllink}}</span>
+                <h3>By {{item.author}}</h3>
+                <div class="bottom clearfix">
+                  <el-button disabled type="text" class="button">Link</el-button>
+              </div>
+            </div>
+            </el-card>
+            <div>
+              
+            </div>
+            </a>
+          </el-col>
+        </el-row>
+        <el-row :gutter='20' v-if='askhn_list'>
           <el-col :span='8' v-for="item in askhn_list">
-                                  <a :href="item.url">
+            <a :href="item.url">
             <el-card :body-style="{ padding: '0px' }">
               <img src='https://media.wired.com/photos/5a55a95af41e4c2cd9ee6cd8/191:100/pass/Medium-logo-canvas.jpg'>
               <div style="padding: 14px;">
@@ -51,22 +71,32 @@ export default {
   data: function() {
     return {
       reddit_list: [],
-      askhn_list: []
+      askhn_list: [],
+      podcasts_list: []
     };
   },
   created() {
-    let that = this;
-    axios
+  },
+  methods: {
+    runReddit: function(){
+      let that = this;
+      that.askhn_list = []
+      axios
       .get("https://api.pushshift.io/reddit/search/submission/?q=javascript")
       .then(response => {
         that.reddit_list = response.data.data;
       });
-    axios
+    },
+    runAskhn: function(){
+      let that = this;
+      that.reddit_list = [];
+      axios
       .get("http://hn.algolia.com/api/v1/search?query=vuejs&tags=story")
       .then(response => {
         that.askhn_list = response.data.hits;
         console.log(that.askhn_list);
       });
+    }
   }
 };
 </script>
