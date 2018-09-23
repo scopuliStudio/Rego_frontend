f8a28bf9cd30fb57d3bb1ec82b85f047a60d48a5
Yernar_Akimzhanov <ernarkazakh07@gmail.com>
2018-09-22 20:40:26 -0400

    initial commit

---
+++ src/components/Main.vue
@@ -0,0 +1,92 @@
+<template>
+    <div>
+      <h2 class='rego'>REGO</h2>
+      <h2 class='front-end-developer'>Front End Developer</h2>
+      <hr style="clear:both;"/>
+      <div>
+        <h3>Articles</h3>
+        <h3>Discussions</h3>
+        <h3>People</h3>
+      </div>
+      <div>
+        <el-row :gutter='20'>
+          <el-col :span='8' v-for="item in askhn_list">
+                                  <a :href="item.url">
+            <el-card :body-style="{ padding: '0px' }">
+              <img src='https://media.wired.com/photos/5a55a95af41e4c2cd9ee6cd8/191:100/pass/Medium-logo-canvas.jpg'>
+              <div style="padding: 14px;">
+                <span>{{item.title}}</span>
+                <span>{{item.fulllink}}</span>
+                <h3>By {{item.author}}</h3>
+                <div class="bottom clearfix">
+                  <el-button disabled type="text" class="button">Link</el-button>
+              </div>
+            </div>
+            </el-card>
+            <div>
+              
+            </div>
+            </a>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      reddit_list: [],
+      askhn_list: []
+    };
+  },
+  created() {
+    let that = this;
+    axios
+      .get("https://api.pushshift.io/reddit/search/submission/?q=javascript")
+      .then(response => {
+        that.reddit_list = response.data.data;
+      });
+    axios
+      .get("http://hn.algolia.com/api/v1/search?query=vuejs&tags=story")
+      .then(response => {
+        that.askhn_list = response.data.hits;
+        console.log(that.askhn_list);
+      });
+  }
+};
+</script>
+<style>
+img {
+  width: 200px;
+}
+.rego {
+  margin-right: 10%;
+  display: inline;
+  height: 60px;
+  width: 136px;
+  color: #000000;
+  font-family: Muli !important;
+  font-size: 48px;
+  font-weight: 600;
+  letter-spacing: 0.2px;
+  line-height: 60px;
+  text-align: center;
+}
+.front-end-developer {
+  display: inline;
+  height: 60px;
+  width: 581px;
+  opacity: 0.76;
+  color: #8c8c8c;
+  font-family: Muli;
+  font-size: 48px;
+  font-weight: 600;
+  letter-spacing: 0.2px;
+  line-height: 60px;
+  text-align: center;
+}
+</style>
+
