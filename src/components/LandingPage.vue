c5fff9b3f50b263507c8da3f94dc05362be740eb
Max Bardus <maksymbardus@gmail.com>
2018-11-15 17:06:56 -0500

    Added deleted app.vue

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,24 +1,54 @@
 <template>
-    <div clas> <!-- this is body jkcidshfhjdb-->
-    <h2>asdasd</h2>
-    </div>
+    <div clas> <!-- this is body-->
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand brand-title">
+      REGO
+  </div>
+  <div class="navbar-menu">
+    <!-- navbar start, navbar end -->
+  </div>
+</nav>
+</div>
+
+    
 </template>
-
-
 
 <style>
 
-html, body {
-    font-family: "Muli", sans-serif;
-    font-size: 16px;
-    background-color: #ffffff;
-    margin: 0;
-    font-size: 16px;
-}
-
-img{
-    width: 100%;
-    height: 100vh;
-}
 
 </style>
+
+<script>
+export default {
+  created(){
+    test:{
+      document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
+      
+    }
+  }
+}
+</script>
