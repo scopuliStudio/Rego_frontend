53860e2855018c838701a3bbbe04042bec11ada1
Max Bardus <maksymbardus@gmail.com>
2018-11-15 16:06:42 -0500

    initial commit

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,24 +1,14 @@
 <template>
-    <div>
-        <img src='../assets/bgr.png'>
-    </div>
-</template>
+    <div>  <!-- Body -->
+        <section class="l-banner">
+            <div class=""
+        </section>
 
 
 
 <style>
-
 html, body {
     font-family: "Muli", sans-serif;
-    font-size: 16px;
-    background-color: #ffffff;
-    margin: 0;
-    font-size: 16px;
-}
-
-img{
-    width: 100%;
     height: 100vh;
-}
 
 </style>
