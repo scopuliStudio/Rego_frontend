ce0bace7c46e651018a82fe3a55d34db65c08e02
Yernar_Akimzhanov <ernarkazakh07@gmail.com>
2018-11-15 16:58:21 -0500

    Deleted Element UI library

--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
 import Vue from 'vue';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/reset.css'
-import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
 import './../node_modules/bulma/css/bulma.css';
 import './assets/style.css'
-
-Vue.use(ElementUI);
 
 new Vue({
   el: '#app',
