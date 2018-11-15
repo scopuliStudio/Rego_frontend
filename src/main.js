6ca8013e103f6633405be811ca5c4f97523b51f5
Yernar_Akimzhanov <ernarkazakh07@gmail.com>
2018-11-15 16:43:48 -0500

    Added bulma css

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/reset.css'
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
+import './../node_modules/bulma/css/bulma.css';
 import './assets/style.css'
 
 Vue.use(ElementUI);
