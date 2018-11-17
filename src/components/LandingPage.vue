eea2af1578f67187ee50fea14ccb882916dfcee1
Max Bardus <maksymbardus@gmail.com>
2018-11-17 15:18:04 -0500

    add complete navigation, banner, hero text

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,19 +1,22 @@
 <template>
-  <div>
+  <div> <!-- Body -->
     <Navbar />
-    <div class="banner">
+     <div class="banner">
       <img src="../assets/banner.svg" class="landing-banner" alt="Banner">
+    </div>
+    <div class="container">
+    <div class="hero-container">
+      <h1 class="hero-text">The way to discover great content <span class="break-text">you were looking for</span></h1>
+      <button class="start-btn">
+        Get Started
+      </button>
+    </div>
     </div>
   </div>
 </template>
 
 <style>
-  
 </style>
-
-
-
-
 
 <script>
 import Navbar from '../components/navigation.vue'
