30bc89625242680f26e7935edbf7d9d5c81e0696
Yernar_Akimzhanov <ernarkazakh07@gmail.com>
2018-11-19 17:53:07 -0500

    trying to make it work with nutlify

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
-import './../node_modules/bulma/css/bulma.css';
-import './assets/style.css'
+import '/../node_modules/bulma/css/bulma.css';
+import '/assets/style.css'
 
 new Vue({
   el: '#app',
