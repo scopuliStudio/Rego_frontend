563d82d8235a6db718d63d60687c4bb56f2488e1
Yernar_Akimzhanov <ernarkazakh07@gmail.com>
2018-11-19 14:37:35 -0500

    changed configs

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <LandingPage />
+  </div>
+</template>
+
+<script>
+import LandingPage from './components/LandingPage.vue'
+
+export default {
+  name: 'app',
+  components: {
+    LandingPage,
+  }
+}
+
+
+
+</script>
